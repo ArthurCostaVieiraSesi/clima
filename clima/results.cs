d047d3467d8c462070346623cc0a043d8b524ea0
ArthurCosta <opsarthur@gmail.com>
2024-04-17 09:42:02 -0300

    jose carlos e eduardo se separaram e ele morreu

---
+++ clima/results.cs
@@ -0,0 +1,17 @@
+namespace clima;
+
+public class results
+{
+    public int temp {get;set;}
+    public string description {get;set;}
+    public string city {get;set;}
+    public double rain {get;set;}
+    public int humidity {get;set;}
+    public double sunrise {get;set;}
+    public double sunset {get;set;}
+    public double wind_speed {get;set;}
+    public double wind_direction {get;set;}
+    public string moon_phase {get;set;}
+    public string codition_code {get;set;}
+    public string img_id {get;set;}
+}
