b87e2b960ff94c47e308d44186c13b34fc21496c
ArthurCosta <opsarthur@gmail.com>
2024-04-18 11:48:23 -0300

    eduardo se casou com jeferson

--- clima/MainPage.xaml.cs
+++ clima/MainPage.xaml.cs
@@ -2,11 +2,46 @@
 
 public partial class MainPage : ContentPage
 {
-
+    Results resultado;
 	public MainPage()
 	{
 		InitializeComponent();
+		TestaLayout();
+		PreencherTela();
 	}
+
+		void TestaLayout()
+		{
+			resultado = new Results();
+			resultado.temp = 24;
+			resultado.description = "Tempo Nublado";
+			resultado.rain = 88;
+			resultado.city = "Apucarana";
+			resultado.humidity = 88;
+			resultado.wind_speed = 3;
+			resultado.wind_direction = 397;
+			resultado.wind_cardinal = "N";
+			resultado.sunrise = "06:13";
+			resultado.sunset = "18:27";
+			resultado.moon_phase = "Cheia";
+
+		}
+
+		void PreencherTela()
+		{
+			LabelChuva.Text = resultado.rain.ToString();
+			LabelHumidade.Text = resultado.humidity.ToString();
+			LabelGraus.Text = resultado.temp.ToString();
+			LabelClima.Text = resultado.description;
+			LabelCidade.Text = resultado.city;
+			LabelForça.Text = resultado. wind_speed.ToString();
+			LabelDireção.Text = resultado.wind_direction.ToString();
+			LabelAmanhecer.Text = resultado.sunrise;
+			LabelAnoitecer.Text = resultado.sunset;
+			LabelLua.Text = resultado.moon_phase;
+			LabelCardinal.Text = resultado.wind_cardinal;
+
+		}
 
 }
 
--- clima/results.cs
+++ clima/results.cs
@@ -1,17 +1,18 @@
 namespace clima;
 
-public class results
+public class Results
 {
     public int temp {get;set;}
     public string description {get;set;}
     public string city {get;set;}
     public double rain {get;set;}
     public int humidity {get;set;}
-    public double sunrise {get;set;}
-    public double sunset {get;set;}
+    public string sunrise {get;set;}
+    public string sunset {get;set;}
     public double wind_speed {get;set;}
     public double wind_direction {get;set;}
     public string moon_phase {get;set;}
     public string codition_code {get;set;}
     public string img_id {get;set;}
+    public string wind_cardinal {get;set;}
 }
